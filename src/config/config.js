df8d1a95bb822935891b8e5c8bf30bc8c5d3c467
Zack Othman <zharifothman92@gmail.com>
2018-03-31 13:36:32 +0800

    My Commist

---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+export const dbconfig = {
+  apiKey: "AIzaSyDcxTX26tAopPaVESmrMpY2jkKydH9_mZg",
+  authDomain: "react-notes-5bb85.firebaseapp.com",
+  databaseURL: "https://react-notes-5bb85.firebaseio.com",
+  projectId: "react-notes-5bb85",
+  storageBucket: "react-notes-5bb85.appspot.com",
+  messagingSenderId: "392544746546"
+};
